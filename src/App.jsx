4c93138b78ae9a6b72622ae62df1a66e64d34869
JotaPrimo <gestald118@gmail.com>
2023-06-27 18:59:02 -0400

    87. Resgatando dados da AP

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const URL_BASE = "http://localhost:3000/products";
+
+  const [produtos, setProdutos] = useState([]);
+
+  // resgantando os dados
+  // useEffecte garante que a ação será executada apenas uma vez
+  // problema da re-renderizacao d ecompoentens
+  useEffect(() => {
+    async function fetchData() {
+      // You can await here
+      const response = await fetch(URL_BASE);
+      const jsonData = await response.json();
+      setProdutos(jsonData);
+      // ...
+    }
+
+    fetchData();
+  }, []); // Or [] if effect doesn't need props or state
+
+  console.log(produtos);
 
   return (
     <>
-     <h1>Lista de Produtos</h1>
+      <h1>Lista de Produtos</h1>
+      <ul>
+        {produtos.map(({id, name, price}) => (
+          <li key={id}> {name} : R$ {price} </li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>  removi o strictr mode para não executar duas vezes o codigo
     <App />
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
