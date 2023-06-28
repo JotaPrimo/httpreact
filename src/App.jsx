0508ad140cf27502489faddb4b94daf4e92305dc
JotaPrimo <gestald118@gmail.com>
2023-06-28 14:17:14 -0400

    94. Tratando erros

--- src/App.jsx
+++ src/App.jsx
@@ -1,108 +1,109 @@
+import "./App.css";
+
 import { useState, useEffect } from "react";
 
-// 4 - importando custom hook
-import { useFecth } from './hooks/useFecth'
+// 4 - custom hook
+import { useFetch } from "./hooks/useFecth";
+
+// 8 - errar url para mostrar erro
+// "http://localhost:3001/products"
+const url = "http://localhost:3000/products";
 
 function App() {
-  // constantes
-  const URL_BASE = "http://localhost:3000/products";
+  const [products, setProducts] = useState([]);
 
-  // states
-  const [produtos, setProdutos] = useState([]);
+  // 4 - custom hook e 5 - refactor post
+  const { data: items, httpConfig, loading, error } = useFetch(url);
 
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
-  // 4 custom hook 
-  // esse : é uma alias, como em bano de dados
-  const { data: items, httpConfig, loading } = useFecth(URL_BASE);
+  // 1 - resgatando dados
+  // useEffect(async () => {
+  //   const res = await fetch("http://localhost:3000/products");
 
-  console.log(items);
+  //   const data = await res.json();
 
+  //   setProducts(data);
+  // }, []);
 
-  const [name, setName] = useState('');
-  const [price, setPrice] = useState('');
+  // 2 - add product
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-  // resgantando os dados
-  // useEffecte garante que a ação será executada apenas uma vez
-  // problema da re-renderizacao d ecompoentens
-  // useEffect(() => {
-  //   async function fetchData() {
-  //     // You can await here
-  //     const response = await fetch(URL_BASE);
-  //     const jsonData = await response.json();
-  //     setProdutos(jsonData);
-  //     // ...
-  //   }
-
-  //   fetchData();
-  // }, []); // Or [] if effect doesn't need props or state
-
-
-
-  // funcao para submter o form
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-
-    const produto = {
+    const product = {
       name,
-      price
-    }
+      price,
+    };
 
-    // const res = await fetch(URL_BASE, {
-    //   method: 'POST',
+    // const res = await fetch("http://localhost:3000/products", {
+    //   method: "POST",
     //   headers: {
     //     "Content-Type": "application/json",
     //   },
+    //   body: JSON.stringify(product),
+    // });
 
-    //   body: JSON.stringify(produto)
-    // })
+    // const addedProduct = await res.json();
 
-    // res não é json, é texto, por isso converti
-    // const addedProduto = await res.json();
+    // 3 - carregamento dinâmico
+    // setProducts((prevProducts) => [...prevProducts, addedProduct]);
 
-    // setProdutos((prevProdutos) => [...prevProdutos, addedProduto]);
+    // 5 - refatorar post
+    httpConfig(product, "POST");
 
-    // refatorando o post
-    httpConfig(produto, "POST")
-
-
-    // resetando inputs
     setName("");
     setPrice("");
+  };
 
-  }
+  /* 9 - desafio */
+  const handleRemove = (id) => {
+    httpConfig(id, "DELETE");
+  };
 
   return (
-    // items && é um if, verifica se existe, depois faz
-    <>
-      <h1>Lista de Produtos</h1>
+    <div className="App">
+      <h1>Lista de produtos</h1>
+      {/* 6 - state de loading */}
+      {loading && <p>Carregando dados...</p>}
+      {error && <p>{error}</p>}
+      <ul>
+        {items &&
+          items.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$: {product.price}
+              {/* 9 - desafio */}
+              <button onClick={() => handleRemove(product.id)}>Excluir</button>
+            </li>
+          ))}
+      </ul>
 
-      {/* 66 loading */}
-      {/** enquanto estiver aguardando dados mostra esse loadin */}
-      { loading && <p > Buscando dados...</p> }
-
-      { !loading && <ul>
-         { items && items.map(({id, name, price}) => (
-          <li key={id}> {name} : R$ {price} </li>
-        ))}
-      </ul> }
-
-      <hr />
-
-      <form onSubmit={handleSubmit}>
-        <label>
-          <span>Name</span>
-          <input type="text" name="name" value={name} onChange={(event) => setName(event.target.value) } />
-        </label>
-
-        <label>
-          <span>Price</span>
-          <input type="text" name="price" value={price} onChange={(event) => setPrice(event.target.value)} />
-        </label>
-
-       { !loading &&  <button type="submit">Salvar</button>}
-
-      </form>
-    </>
+      <div className="add-product">
+        <p>Adicionar produto:</p>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={name}
+              name="name"
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="number"
+              value={price}
+              name="price"
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          {/* 7 - state de loading no post */}
+          {loading ? <p>Aguarde!</p> : <input type="submit" value="Criar" />}
+        </form>
+      </div>
+    </div>
   );
 }
 
--- src/hooks/useFecth.jsx
+++ src/hooks/useFecth.jsx
@@ -1,109 +1,109 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
-// hook deve ser exportado como funcao
-// só posso ter um export
-
-// custon hook para o fetch de dados
-export const useFecth = (url) => {
-  const GET = "GET";
-  const POST = "POST";
-  const PATCH = "PATCH";
-  const PUT = "PUT";
-  const DELETE = "DELETE";
-
-  // check type method
-  function isGet() {
-    return method === GET;
-  }
-
-
-  function isPost() {
-    return method === POST;
-  }
-
-  function isPut() {
-    return method === PUT;
-  }
-
-  function isDelete() {
-    return method === DELETE;
-  }
-
-  // check type method
-
+// 4 - custom hook
+export const useFetch = (url) => {
   const [data, setData] = useState(null);
 
-  // useEffetc para executar apenas uma vez
-
-  // 5 refatorando o post
-  // vamos configrar o cabelho das req's aqui
+  // 5 - refatorando post
   const [config, setConfig] = useState(null);
   const [method, setMethod] = useState(null);
   const [callFetch, setCallFetch] = useState(false);
 
+  // 6 - estado de loading
+  const [loading, setLoading] = useState(false);
 
-  // 6 - loading
-  const [loading, setLoading] = useState(false); 
+  // 8 - tratando erros
+  const [error, setError] = useState(false);
 
+  // 9 - desafio
+  const [itemId, setItemId] = useState(null);
 
-  // configurando http config
   const httpConfig = (data, method) => {
-    if (isPost) {
-        setConfig({
-            method,
-            headers: {
-                "Content-type": "application/json"
-            },           
-            body: JSON.stringify(data)    
-        })
+    if (method === "POST") {
+      setConfig({
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-        setMethod(method);
+      setMethod("POST");
+    } else if (method === "DELETE") {
+      setConfig({
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      setMethod("DELETE");
+      setItemId(data);
     }
-
-  }
+  };
 
   useEffect(() => {
     const fetchData = async () => {
+      // 6 - estado de loading
+      // mudar throttling no network p 3g
+      setLoading(true);
 
-        // 6 loading
-setLoading(true);
+      // 8 - tratando erros
+      try {
+        const res = await fetch(url);
 
+        const json = await res.json();
 
-      // primeiro vem em texto
-      const res = await fetch(url);
+        setData(json);
 
-      // convertendo para json
-      const json = await res.json();
+        setMethod(null);
 
-      // setando os dados
-      setData(json);
+        // 8 - tratando erros
+        setError(null);
+      } catch (error) {
+        console.log(error.message);
+
+        setError("Houve um erro ao carregar os dados!");
+      }
+
+      setLoading(false);
     };
 
-    // executando minha funcao de fech data
     fetchData();
-
-    setLoading(false);
-
   }, [url, callFetch]);
 
-  // 5 refatorando o post
+  // 5 - refatorando post
   useEffect(() => {
     const httpRequest = async () => {
-        if (isPost) {
-            let fetchOptions = [url ,config];
-    
-            const res = await fetch(...fetchOptions);
-    
-            const json = await res.json();
-    
-            setCallFetch(json)
-        }
-    } ;
-    
-    // executando a funcao
+      if (method === "POST") {
+        // 7 - loading no post
+        // mudar throttling no network p 3g
+        setLoading(true);
+
+        // 5 - refatorando post
+        let fetchOptions = [url, config];
+
+        const res = await fetch(...fetchOptions);
+
+        const json = await res.json();
+
+        setCallFetch(json);
+        // 9 - desafio
+      } else if (method === "DELETE") {
+        const deleteUrl = `${url}/${itemId}`;
+
+        const res = await fetch(deleteUrl, config);
+
+        const json = await res.json();
+
+        setCallFetch(json);
+      }
+    };
+
     httpRequest();
+  }, [config]);
 
-  }, [config, method, url]);
+  console.log(config);
 
-  return { data, httpConfig, loading };
+  return { data, httpConfig, loading, error };
 };
